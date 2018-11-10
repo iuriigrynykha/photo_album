83568bf9150b4a499dda8994a264add00aa31dd9
Юрий Владимирович <geroge.gru@gmail.com>
2018-11-10 16:22:24 +0200

    user saga

--- src/sagas/usersSaga.js
+++ src/sagas/usersSaga.js
@@ -1,9 +1,9 @@
-// import { call, put } from "redux-saga/effects";
-// import { userLoggedIn } from "../actions/auth";
-// import api from "../api";
+import { call, put } from "redux-saga/effects";
+import { userLoggedIn } from "../actions/auth";
+import api from "../api";
 
-// export default function* createUserSaga(action) {
-//   const user = yield call(api.user.login, action.user);
-//   localStorage.user = JSON.stringify(user);
-//   yield put(userLoggedIn(user));
-// }
+export default function* createUserSaga(action) {
+  const user = yield call(api.user.login, action.user);
+  localStorage.user = JSON.stringify(user);
+  yield put(userLoggedIn(user));
+}
